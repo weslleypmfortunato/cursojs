7687660d1034d131597df3723647658709f3ebf5
weslleypmfortunato <111170704+weslleypmfortunato@users.noreply.github.com>
2022-09-11 21:33:26 -0700

    fim JS

--- aula14ex/ex017/scrip.js
+++ aula14ex/ex017/scrip.js
@@ -1,1 +1,21 @@
-alert('Ola!')
+function tabuada() {
+  let num = document.getElementById('txtn')
+  let tab = document.getElementById('seltab')
+  if (num.value.length == 0) {
+    window.alert('Por favor, digite um número!')
+  } else {
+    let n = Number(num.value)
+
+    let c = 1
+    tab.innerHTML = '' // para limpar a tabuada antes de mostrar
+    while (c <=10) {
+      let item = document.createElement('option')
+      item.text = `${n} x ${c} = ${n * c}`
+      item.value = `tab${c}`
+      c++
+      tab.appendChild(item)
+    }
+  }
+
+
+}
---
+++ aula16/funcao02.js
@@ -0,0 +1,7 @@
+function soma(n1=0, n2=0) {
+  return n1 + n2
+}
+
+console.log(soma(8))
+
+//console.log(`A soma de ${n1} + ${n2} é igual a ${n1 + n2}`)
